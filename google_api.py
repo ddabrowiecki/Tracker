5cb4ed4a5ddb80d22c675bb29c6047bf23c8e060
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2024-04-10 13:19:46 -0500

    Update google_api.py

--- google_api.py
+++ google_api.py
@@ -11,7 +11,7 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 SPREADSHEET_ID = '1_xsDfFHZSgGANCFFVZGdPMnaCLlDHIB7xi4XYvZz7yQ'
 RANGE = 'Tracker!A2:B2'
 
-f = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+f = open(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 key = json.loads(f)
 print(key)
 
