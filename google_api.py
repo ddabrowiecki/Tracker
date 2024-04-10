beb1fcf4913a555deacb0ca865ae44b687182a74
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2024-04-10 12:44:18 -0500

    Update google_api.py

--- google_api.py
+++ google_api.py
@@ -18,7 +18,7 @@ def append_price(values, value_input_option="USER_ENTERED"):
     """
     Add price to spreadsheet
     """
-    credentials = service_account.Credentials.from_service_account_file()
+    credentials = service_account.Credentials.from_service_account_file(KEY)
     append_body = {
         "values": values,
         "range": RANGE,
