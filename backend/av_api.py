eefc10f44d7f5d0226a01351a50e8a5e7335a40a
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2024-04-17 12:22:38 -0500

    Update AV api file

--- backend/av_api.py
+++ backend/av_api.py
@@ -1,6 +1,6 @@
 import requests
 import os
-from OptTracker.backend.google_api import append_price
+from backend.google_api import append_price
 import datetime
 
 key = os.environ["AV_KEY"]
@@ -10,7 +10,21 @@ TODAY_DATE = datetime.datetime.now().strftime("%x")
 def get_quote_close():
     url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=RDDT&apikey={key}'
     r = requests.get(url)
-    quote = r.json()
-    return quote['Global Quote']['05. price']
+    quote = r.json()['Global Quote']
+    return quote
 
-append_price([[TODAY_DATE, get_quote_close()]])
+stock_data = get_quote_close()
+
+append_price([
+    [
+        TODAY_DATE, 
+        stock_data['05. price'],
+        stock_data['02. open'],
+        stock_data['03. high'],
+        stock_data['04. low'],
+        stock_data['06. volume'],
+        stock_data['08. previous close'],
+        stock_data['09. change'],
+        stock_data['10. change percent'],
+        ]
+    ])
